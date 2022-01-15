28c2bc435b3458fece27c84a9024932626ddbd1a
zoard-ehmann <zoard.ehmann@gmail.com>
2022-01-15 14:31:50 +0100

    Add key listeners to the game to control snake

--- Day20/Snake_Game/main.py
+++ Day20/Snake_Game/main.py
@@ -10,6 +10,12 @@ screen.tracer(0)
 
 snake = Snake()
 
+screen.listen()
+screen.onkey(key="Up", fun=snake.up)
+screen.onkey(key="Down", fun=snake.down)
+screen.onkey(key="Left", fun=snake.left)
+screen.onkey(key="Right", fun=snake.right)
+
 game_is_on = True
 while game_is_on:
     screen.update()
--- Day20/Snake_Game/snake.py
+++ Day20/Snake_Game/snake.py
@@ -2,6 +2,10 @@ from turtle import Turtle
 
 STARTING_POSITONS = [(0, 0), (-20, 0), (-40, 0)]
 MOVE_DISTANCE = 20
+UP = 90
+DOWN = 270
+LEFT = 180
+RIGHT = 0
 
 class Snake:
 
@@ -9,6 +13,7 @@ class Snake:
     def __init__(self):
         self.segments = []
         self.create_snake()
+        self.snake_head = self.segments[0]
 
 
     def create_snake(self):
@@ -24,4 +29,30 @@ class Snake:
         for seg_num in range(len(self.segments) - 1, 0, -1):
             new_position = self.segments[seg_num - 1].position()
             self.segments[seg_num].goto(new_position)
-        self.segments[0].forward(MOVE_DISTANCE)
+        self.snake_head.forward(MOVE_DISTANCE)
+
+
+    def __valid_direction(self, new_direction):
+        if abs(self.snake_head.heading() - new_direction) == 180:
+            return False
+        return True
+
+
+    def up(self):
+        if self.__valid_direction(UP):
+            self.snake_head.setheading(UP)
+
+
+    def down(self):
+        if self.__valid_direction(DOWN):
+            self.snake_head.setheading(DOWN)
+
+
+    def left(self):
+        if self.__valid_direction(LEFT):
+            self.snake_head.setheading(LEFT)
+
+
+    def right(self):
+        if self.__valid_direction(RIGHT):
+            self.snake_head.setheading(RIGHT)
