8e44d3c2fcf3301b3e6fefd085b2f365fc22a623
zoard-ehmann <zoard.ehmann@gmail.com>
2022-01-16 07:53:30 +0100

    Adding collision detection with food

---
+++ Day21/Snake_Game/main.py
@@ -0,0 +1,31 @@
+from turtle import Screen
+from snake import Snake
+from food import Food
+import time
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title("Snake Game")
+screen.tracer(0)
+
+snake = Snake()
+food = Food()
+
+screen.listen()
+screen.onkey(key="Up", fun=snake.up)
+screen.onkey(key="Down", fun=snake.down)
+screen.onkey(key="Left", fun=snake.left)
+screen.onkey(key="Right", fun=snake.right)
+
+game_is_on = True
+while game_is_on:
+    screen.update()
+    time.sleep(0.1)
+    snake.move()
+    
+    # Detect collision with food
+    if snake.snake_head.distance(food) < 15:
+        food.refresh()
+
+screen.exitonclick()
