b934fcbd000a2191d5d9bd06a75d8d5a450ae0b3
zoard-ehmann <zoard.ehmann@gmail.com>
2022-01-17 08:39:18 +0100

    Create a paddle and move it

--- Day22/Pong_Game/main.py
+++ Day22/Pong_Game/main.py
@@ -1,10 +1,34 @@
-from turtle import Screen
+from turtle import Turtle, Screen
+
+DISTANCE = 20
+
+
+def move_up():
+    paddle.sety(paddle.ycor() + DISTANCE)
+
+
+def move_down():
+    paddle.sety(paddle.ycor() - DISTANCE)
+
 
 screen = Screen()
 
 screen.setup(width=800, height=600)
 screen.bgcolor("black")
 screen.title("Pong Game")
+screen.tracer(0)
+screen.listen()
+screen.onkey(key="Up", fun=move_up)
+screen.onkey(key="Down", fun=move_down)
 
+paddle = Turtle("square")
+paddle.color("white")
+paddle.penup()
+paddle.turtlesize(stretch_wid=5, stretch_len=1)
+paddle.setx(350)
+
+game_is_on = True
+while game_is_on:
+    screen.update()
 
 screen.exitonclick()
