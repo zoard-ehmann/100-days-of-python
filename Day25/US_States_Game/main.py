837cbcb7584b2e721b6fbfb487217e8910db269f
zoard-ehmann <zoard.ehmann@gmail.com>
2022-01-20 19:31:45 +0100

    Implement crucial functions of US States Game

---
+++ Day25/US_States_Game/main.py
@@ -0,0 +1,38 @@
+import turtle
+
+import pandas
+
+
+IMAGE = "Day25/US_States_Game/blank_states_img.gif"
+STATES = "./Day25/US_States_Game/50_states.csv"
+
+
+def write_state(state_data):
+    pen = turtle.Turtle()
+    pen.hideturtle()
+    pen.penup()
+    pen.goto(int(state_data.x), int(state_data.y))
+    pen.write(f"{state_data.state.item()}")
+
+
+screen = turtle.Screen()
+screen.title("U.S. States Game")
+screen.addshape(IMAGE)
+turtle.shape(IMAGE)
+screen.tracer(0)
+
+states_df = pandas.read_csv(STATES)
+number_of_states = len(states_df.state)
+guessed_states = []
+
+while len(guessed_states) < number_of_states:
+    answer_state = screen.textinput(title=f"{len(guessed_states)}/{number_of_states} States Correct", prompt="Tell a state's name.").title()
+    guess = states_df[states_df.state == answer_state]
+    
+    if not guess.empty and answer_state not in guessed_states:
+        write_state(guess)
+        guessed_states.append(answer_state)
+        
+    screen.update()
+
+screen.exitonclick()
