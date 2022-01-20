43c7abaa783794c3ba885b0f5d10cb40b593104b
zoard-ehmann <zoard.ehmann@gmail.com>
2022-01-20 08:04:00 +0100

    Basic pandas operations

--- Day25/Reading_CSV/main.py
+++ Day25/Reading_CSV/main.py
@@ -17,6 +17,43 @@
     
 import pandas
 
-
+# DataFrame
 data = pandas.read_csv("./Day25/Reading_CSV/weather_data.csv")
+
+# Series
 print(data["temp"])
+
+data_dict = data.to_dict()
+print(data_dict)
+
+temp_list = data["temp"].to_list()
+print(len(temp_list))
+
+print(data["temp"].mean())
+print(data["temp"].max())
+
+# Get Data in Columns
+print(data["condition"])    # Treating data as a dictionary
+print(data.condition)       # Treating data as an object
+
+# Get Data in Row
+print(data[data.day == "Monday"])
+
+# Get the day which has the highest temperature
+print(data[data.temp == data.temp.max()])
+
+monday = data[data.day == "Monday"]
+print(monday.condition)
+
+monday_temp = int(monday.temp)
+monday_temp_F = monday_temp * 9 / 5 + 32
+print(monday_temp_F)
+
+# Create a data frame from scratch
+data_dict = {
+    "students": ["Amy", "James", "Angela"],
+    "scores": [76, 56, 65]
+}
+
+data = pandas.DataFrame(data_dict)
+data.to_csv("./Day25/Reading_CSV/new_data.csv")
