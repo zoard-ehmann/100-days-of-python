2753687916a35a5796b09c844dd88b96e5513c0f
zoard-ehmann <zoard.ehmann@gmail.com>
2022-01-22 10:47:12 +0100

    Housekeeping

--- Day27/GUI/main.py
+++ Day27/GUI/main.py
@@ -1,5 +1,10 @@
 from tkinter import *
 
+
+def button_clicked():
+    my_label.config(text=input.get())
+
+
 window = Tk()
 window.title("My First GUI Program")
 window.minsize(width=500, height=300)
@@ -8,19 +13,11 @@ window.minsize(width=500, height=300)
 my_label = Label(text="Just a Label", font=("Arial", 24, "bold"))
 my_label.pack()
 
-my_label["text"] = "New Text"
-my_label.config(text="New Text")
-
 # Button
-
-def button_clicked():
-    my_label.config(text=input.get())
-
 button = Button(text="Click Me", command=button_clicked)
 button.pack()
 
 # Entry
-
 input = Entry(width=10)
 input.pack()
 
