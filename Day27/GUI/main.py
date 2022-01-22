8d38e20fe170ea01ee6cc312e5620985c6827978
zoard-ehmann <zoard.ehmann@gmail.com>
2022-01-22 10:28:53 +0100

    Basic TKInter widgets

--- Day27/GUI/main.py
+++ Day27/GUI/main.py
@@ -1,12 +1,32 @@
-import tkinter
+from tkinter import *
 
-window = tkinter.Tk()
+window = Tk()
 window.title("My First GUI Program")
 window.minsize(width=500, height=300)
 
 # Label
-my_label = tkinter.Label(text="Just a Label", font=("Arial", 24, "bold"))
-my_label.pack(side="left")
+my_label = Label(text="Just a Label", font=("Arial", 24, "bold"))
+my_label.pack()
+
+my_label["text"] = "New Text"
+my_label.config(text="New Text")
+
+# Button
+
+def button_clicked():
+    my_label.config(text=input.get())
+
+button = Button(text="Click Me", command=button_clicked)
+button.pack()
+
+# Entry
+
+input = Entry(width=10)
+input.pack()
+
+
+
+
 
 
 window.mainloop()
