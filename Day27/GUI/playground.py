36b0f8d207ea834bb1e8755b112815028e6c5eba
zoard-ehmann <zoard.ehmann@gmail.com>
2022-01-22 10:02:36 +0100

    Many keyword arguments example

--- Day27/GUI/playground.py
+++ Day27/GUI/playground.py
@@ -3,3 +3,10 @@ def add(*args):
     return sum(n for n in args)
 
 print(add(1, 5, 3, 8))
+
+def calculate(n, **kwargs):
+    n += kwargs["add"]
+    n *= kwargs["multiply"]
+    print(n)
+
+calculate(2, add=3, multiply=5)
