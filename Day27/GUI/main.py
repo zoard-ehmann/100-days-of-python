f9d27bd7ed2cf5b0407993afdf0b3325433826c4
zoard-ehmann <zoard.ehmann@gmail.com>
2022-01-22 11:02:20 +0100

    Layout management

--- Day27/GUI/main.py
+++ Day27/GUI/main.py
@@ -8,18 +8,23 @@ def button_clicked():
 window = Tk()
 window.title("My First GUI Program")
 window.minsize(width=500, height=300)
+window.config(padx=100, pady=200)
 
 # Label
 my_label = Label(text="Just a Label", font=("Arial", 24, "bold"))
-my_label.pack()
+my_label.grid(column=0, row=0)
+my_label.config(padx=50, pady=50)
 
-# Button
-button = Button(text="Click Me", command=button_clicked)
-button.pack()
+# Buttons
+button1 = Button(text="Click Me", command=button_clicked)
+button1.grid(column=1, row=1)
+
+button2 = Button(text="Another", command=button_clicked)
+button2.grid(column=2, row=0)
 
 # Entry
 input = Entry(width=10)
-input.pack()
+input.grid(column=3, row=2)
 
 
 
