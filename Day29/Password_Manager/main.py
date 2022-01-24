e14317fb60126b249d0a18894935037d993d6af4
zoard-ehmann <zoard.ehmann@gmail.com>
2022-01-24 08:23:50 +0100

    Deisgn changes and housekeeping

--- Day29/Password_Manager/main.py
+++ Day29/Password_Manager/main.py
@@ -1,6 +1,4 @@
 from tkinter import *
-from turtle import width
-
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
@@ -16,29 +14,28 @@ canvas = Canvas(width=200, height=200, highlightthickness=0)
 canvas.create_image(100, 100, image=logo)
 canvas.grid(row=0, column=1)
 
-label_website = Label(text="Website:")
-label_website.grid(row=1, column=0)
+# Labels
+label_website = Label(text="Website:", width=15, anchor="e")
+label_username = Label(text="Email / Username:", width=15, anchor="e")
+label_password = Label(text="Password:", width=15, anchor="e")
 
+# Entries
 entry_website = Entry(width=54)
-entry_website.grid(row=1, column=1, columnspan=2)
-
-label_username = Label(text="Email / Username:")
-label_username.grid(row=2, column=0)
-
 entry_username = Entry(width=54)
-entry_username.grid(row=2, column=1, columnspan=2)
-
-label_password = Label(text="Password:")
-label_password.grid(row=3, column=0)
-
 entry_password = Entry(width=36)
-entry_password.grid(row=3, column=1)
 
+# Buttons
 button_generate = Button(text="Generate Password")
+button_add = Button(text="Add", width=46)
+
+# Layout
+label_website.grid(row=1, column=0)
+entry_website.grid(row=1, column=1, columnspan=2)
+label_username.grid(row=2, column=0)
+entry_username.grid(row=2, column=1, columnspan=2)
+label_password.grid(row=3, column=0)
+entry_password.grid(row=3, column=1)
 button_generate.grid(row=3, column=2)
-
-button_add = Button(text="Add", width=60)
-button_add.grid(row=4, column=0, columnspan=3, pady=10)
-
+button_add.grid(row=4, column=1, columnspan=2, pady=10)
 
 window.mainloop()
