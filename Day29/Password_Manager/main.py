b23bb6fca721997ee7826de85279bf9be475cfda
zoard-ehmann <zoard.ehmann@gmail.com>
2022-01-24 17:13:40 +0100

    Generate password functionality

--- Day29/Password_Manager/main.py
+++ Day29/Password_Manager/main.py
@@ -1,8 +1,28 @@
 from tkinter import *
 from tkinter import messagebox
+from random import choice, randint, shuffle
+
+import pyperclip
 
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
+def generate_password():
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+    password_list = []
+
+    password_list += [choice(letters) for _ in range(randint(8, 10))]
+    password_list += [choice(symbols) for _ in range(randint(2, 4))]
+    password_list += [choice(numbers) for _ in range(randint(2, 4))]
+
+    shuffle(password_list)
+    password = "".join(password_list)
+    
+    entry_password.insert(0, password)
+    pyperclip.copy(password)
+
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 def save():
@@ -21,6 +41,7 @@ def save():
             entry_website.delete(0, END)
             entry_password.delete(0, END)
             entry_website.focus()
+
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
@@ -43,7 +64,7 @@ entry_username = Entry(width=54)
 entry_password = Entry(width=35)
 
 # Buttons
-button_generate = Button(text="Generate Password")
+button_generate = Button(text="Generate Password", command=generate_password)
 button_add = Button(text="Add", width=45, command=save)
 
 # Layout
@@ -52,7 +73,7 @@ entry_website.grid(row=1, column=1, columnspan=2)
 entry_website.focus()
 label_username.grid(row=2, column=0)
 entry_username.grid(row=2, column=1, columnspan=2)
-entry_username.insert(END, "mail@example.com")
+entry_username.insert(0, "mail@example.com")
 label_password.grid(row=3, column=0)
 entry_password.grid(row=3, column=1)
 button_generate.grid(row=3, column=2)
