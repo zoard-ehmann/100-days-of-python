1aa37a302ae7f5fa36807c3ca6b26bbbf8842a8d
zoard-ehmann <zoard.ehmann@gmail.com>
2022-01-25 15:38:25 +0100

    Implement search functionality

--- Day29/Password_Manager/main.py
+++ Day29/Password_Manager/main.py
@@ -27,7 +27,7 @@ def generate_password():
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 def save():
-    website = entry_website.get()
+    website = entry_website.get().capitalize()
     username = entry_username.get()
     password = entry_password.get()
     new_data = {
@@ -46,6 +46,8 @@ def save():
                 data.update(new_data)
         except FileNotFoundError:
             data = new_data
+        except json.JSONDecodeError:
+            data = new_data
         finally:
             with open("Day29/Password_Manager/data.json", mode="w") as data_file:
                 json.dump(data, data_file, indent=4)
@@ -53,6 +55,26 @@ def save():
         entry_website.delete(0, END)
         entry_password.delete(0, END)
         entry_website.focus()
+
+
+# ---------------------------- FIND PASSWORD ------------------------------- #
+def find_password():
+    website = entry_website.get().capitalize()
+
+    try:
+        with open("Day29/Password_Manager/data.json", mode="r") as data_file:
+            data = json.load(data_file)
+            if website in data:
+                username = data[website]["username"]
+                password = data[website]["password"]
+                pyperclip.copy(password)
+                messagebox.showinfo(title=website, message=f"Username: {username}\nPassword: {password}\n\nPassword has been copied to clipboard.")
+            else:
+                messagebox.showinfo(title="Not Found", message=f"No data found for this website: '{website}'")
+    except FileNotFoundError:
+        messagebox.showerror(title="Keystore Error", message="Keystore file does not exist")
+    except json.JSONDecodeError:
+        messagebox.showerror(title="Empty Keystore", message="Keystore file is empty")
 
 
 # ---------------------------- UI SETUP ------------------------------- #
@@ -71,18 +93,20 @@ label_username = Label(text="Email / Username:", width=15, anchor="e")
 label_password = Label(text="Password:", width=15, anchor="e")
 
 # Entries
-entry_website = Entry(width=54)
+entry_website = Entry(width=35)
 entry_username = Entry(width=54)
 entry_password = Entry(width=35)
 
 # Buttons
+button_search = Button(text="Search", width=14, command=find_password)
 button_generate = Button(text="Generate Password", command=generate_password)
 button_add = Button(text="Add", width=45, command=save)
 
 # Layout
 label_website.grid(row=1, column=0)
-entry_website.grid(row=1, column=1, columnspan=2)
+entry_website.grid(row=1, column=1)
 entry_website.focus()
+button_search.grid(row=1, column=2)
 label_username.grid(row=2, column=0)
 entry_username.grid(row=2, column=1, columnspan=2)
 entry_username.insert(0, "mail@example.com")
