cf228796a9b11c99e1384ba4dbe4240a69fac497
zoard-ehmann <zoard.ehmann@gmail.com>
2022-01-27 13:59:18 +0100

    Datetime module basics

--- Day32/Birthday_Wisher/main.py
+++ Day32/Birthday_Wisher/main.py
@@ -1,19 +1,31 @@
-import smtplib
-import os
+# import smtplib
+# import os
+# 
+# from dotenv import load_dotenv
+# 
+# 
+# load_dotenv()
+# 
+# my_email = os.getenv("MY_EMAIL")
+# password = os.getenv("PASSWORD")
+# 
+# with smtplib.SMTP(os.getenv("SMTP"), os.getenv("PORT")) as connection:
+#     connection.starttls()
+#     connection.login(user=my_email, password=password)
+#     connection.sendmail(
+#         from_addr=my_email,
+#         to_addrs=os.getenv("TARGET_EMAIL"),
+#         msg="Subject:Hello\n\nThis is the body of the mail."
+#     )
 
-from dotenv import load_dotenv
+import datetime as dt
 
 
-load_dotenv()
+now = dt.datetime.now()
+year = now.year
+month = now.month
+day_of_week = now.weekday()
+print(day_of_week)
 
-my_email = os.getenv("MY_EMAIL")
-password = os.getenv("PASSWORD")
-
-with smtplib.SMTP(os.getenv("SMTP"), os.getenv("PORT")) as connection:
-    connection.starttls()
-    connection.login(user=my_email, password=password)
-    connection.sendmail(
-        from_addr=my_email,
-        to_addrs=os.getenv("TARGET_EMAIL"),
-        msg="Subject:Hello\n\nThis is the body of the mail."
-    )
+date_of_birth = dt.datetime(year=1990, month=1, day=7, hour=4)
+print(date_of_birth)
