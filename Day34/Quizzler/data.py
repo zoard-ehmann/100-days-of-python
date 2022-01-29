20f8056004d625c016696f1b4905bbe39ead9262
zoard-ehmann <zoard.ehmann@gmail.com>
2022-01-29 09:04:51 +0100

    Structure project, unescape HTML entitites

--- Day34/Quizzler/data.py
+++ Day34/Quizzler/data.py
@@ -8,7 +8,7 @@ PARAMETERS = {
 }
 
 
-response = requests.get(API_ENDPOINT, PARAMETERS)
+response = requests.get(url=API_ENDPOINT, params=PARAMETERS)
 response.raise_for_status()
 
 question_data = response.json()["results"]
--- Day34/Quizzler/main.py
+++ Day34/Quizzler/main.py
@@ -4,16 +4,11 @@ from quiz_brain import QuizBrain
 
 question_bank = []
 for question in question_data:
-    question_text = question["question"]
-    question_answer = question["correct_answer"]
-    new_question = Question(question_text, question_answer)
-    question_bank.append(new_question)
-
+    question_bank.append(Question(question["question"], question["correct_answer"]))
 
 quiz = QuizBrain(question_bank)
-
 while quiz.still_has_questions():
     quiz.next_question()
 
-print("You've completed the quiz")
-print(f"Your final score was: {quiz.score}/{quiz.question_number}")
+print("You've completed the quiz.")
+print(f"Your final score was: {quiz.score}/{quiz.question_number}.")
