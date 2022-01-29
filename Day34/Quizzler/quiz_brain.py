7aadbf195f0d1842a504783ef4a0ff2fd30f19a4
zoard-ehmann <zoard.ehmann@gmail.com>
2022-01-29 09:07:19 +0100

    Cleaning code

--- Day34/Quizzler/quiz_brain.py
+++ Day34/Quizzler/quiz_brain.py
@@ -3,16 +3,13 @@ import html
 
 class QuizBrain:
 
-
     def __init__(self, question_list):
         self.question_number = 0
         self.question_list = question_list
         self.score = 0
 
-
     def still_has_questions(self):
         return self.question_number < len(self.question_list)
-
 
     def next_question(self):
         question = self.question_list[self.question_number]
@@ -20,7 +17,6 @@ class QuizBrain:
         sanitized_q_text = html.unescape(question.text)
         user_answer = input(f"Q.{self.question_number}: {sanitized_q_text} (True/False)?: ")
         self.check_answer(user_answer, question.answer)
-
 
     def check_answer(self, user_answer, correct_answer):
         if user_answer.lower() == correct_answer.lower():
