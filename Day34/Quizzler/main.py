2238ce3cfc9486966d407387cfb8e91f29b834d4
zoard-ehmann <zoard.ehmann@gmail.com>
2022-01-29 10:06:34 +0100

    Create UI

--- Day34/Quizzler/main.py
+++ Day34/Quizzler/main.py
@@ -1,15 +1,18 @@
 from question_model import Question
 from data import question_data
 from quiz_brain import QuizBrain
+from ui import QuizInterface
 
 
 question_bank = []
+quiz = QuizBrain(question_bank)
+quiz_ui = QuizInterface()
+
 for question in question_data:
     question_bank.append(Question(question["question"], question["correct_answer"]))
 
-quiz = QuizBrain(question_bank)
-while quiz.still_has_questions():
-    quiz.next_question()
+# while quiz.still_has_questions():
+#     quiz.next_question()
 
 print("You've completed the quiz.")
 print(f"Your final score was: {quiz.score}/{quiz.question_number}.")
--- Day34/Quizzler/ui.py
+++ Day34/Quizzler/ui.py
@@ -1,2 +1,31 @@
-THEME_COLOR = "#375362"
+import tkinter as tk
 
+
+THEME_COLOR = "#375362"
+FONT = ("Arial", 16, "italic")
+PADDING = 30
+
+
+class QuizInterface():
+    
+    def __init__(self):
+        self.window = tk.Tk()
+        self.window.title("Quizzler")
+        self.window.config(bg=THEME_COLOR, padx=PADDING, pady=PADDING)
+        
+        self.scoreboard = tk.Label(bg=THEME_COLOR, fg="white", text=f"Score: 0")
+        self.scoreboard.grid(row=0, column=1)
+        
+        self.question_box = tk.Canvas(width=300, height=250, bg="white")
+        self.question_text = self.question_box.create_text(150, 125, text="This will be the question now.", font=FONT, fill=THEME_COLOR, width=275)
+        self.question_box.grid(row=1, column=0, columnspan=2, pady=PADDING)
+        
+        false_image = tk.PhotoImage(file="Day34/Quizzler/images/false.png")
+        self.false_btn = tk.Button(image=false_image, highlightthickness=0, bd=0)
+        self.false_btn.grid(row=2, column=0)
+        
+        true_image = tk.PhotoImage(file="Day34/Quizzler/images/true.png")
+        self.true_btn = tk.Button(image=true_image, highlightthickness=0, bd=0)
+        self.true_btn.grid(row=2, column=1)
+        
+        self.window.mainloop()
