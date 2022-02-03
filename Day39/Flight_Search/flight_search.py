a7a6fd920870190ecda1e0d4eeb4a8ac534fd67c
zoard-ehmann <zoard.ehmann@gmail.com>
2022-02-03 08:40:22 +0100

    Get city codes from city names

--- Day39/Flight_Search/flight_search.py
+++ Day39/Flight_Search/flight_search.py
@@ -1,3 +1,37 @@
+import os
+
+import requests
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+TQ_API = "https://tequila-api.kiwi.com"
+TQ_HEADER = {
+    "apikey": os.getenv("TQ_KEY"),
+}
+
+
 class FlightSearch:
     #This class is responsible for talking to the Flight Search API.
-    pass
+    def __init__(self):
+        self.city_codes = []
+    
+    def get_city_codes(self, cities: list):
+        """Collects the city codes of the passed in cities into a list
+
+        Args:
+            cities (list): a list of city names
+        """
+        
+        tq_params = {
+            "location_types": "city",
+            "limit": 1,
+        }
+        
+        for city in cities:
+            tq_params["term"] = city
+            with requests.Session() as session:
+                response = session.get(url=f"{TQ_API}/locations/query", params=tq_params, headers=TQ_HEADER)
+                response.raise_for_status()
+                self.city_codes.append(response.json()["locations"][0]["code"])
--- Day39/Flight_Search/main.py
+++ Day39/Flight_Search/main.py
@@ -1,1 +1,6 @@
 #This file will need to use the DataManager,FlightSearch, FlightData, NotificationManager classes to achieve the program requirements.
+from flight_search import FlightSearch
+
+flight_search = FlightSearch()
+flight_search.get_city_codes(["Paris", "London", "Tokyo"])
+print(flight_search.city_codes)
