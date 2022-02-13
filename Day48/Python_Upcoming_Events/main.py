b6598dc884a841e094b77f1876717760816f19d1
zoard-ehmann <zoard.ehmann@gmail.com>
2022-02-13 10:26:48 +0100

    Get Python upcoming events using Selenium

---
+++ Day48/Python_Upcoming_Events/main.py
@@ -0,0 +1,30 @@
+import pprint
+
+from selenium import webdriver
+from selenium.webdriver.firefox.service import Service
+from webdriver_manager.firefox import GeckoDriverManager
+from selenium.webdriver.common.by import By
+
+
+PY_URL = 'https://python.org'
+
+
+pp = pprint.PrettyPrinter()
+driver = webdriver.Firefox(service=Service(GeckoDriverManager().install(), log_path='Day48/Python_Upcoming_Events/geckodriver.log'))
+
+driver.get(PY_URL)
+
+event_menu = driver.find_element(By.CSS_SELECTOR, '.event-widget .menu')
+event_dates = event_menu.find_elements(By.TAG_NAME, 'time')
+event_names = event_menu.find_elements(By.TAG_NAME, 'a')
+
+upcoming_events = {
+    nr: {
+        'time':event_dates[nr].text,
+        'name':event_names[nr].text
+    } for nr in range(len(event_names))
+}
+
+pp.pprint(upcoming_events)
+
+driver.quit()
