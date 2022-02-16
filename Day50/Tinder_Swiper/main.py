81a4b629af306013c40bfc49f3c349b970331e3d
Zoárd Ehmann <zoard.ehmann@gmail.com>
2022-02-16 08:13:08 +0100

    Automatic Tinder swiper

--- Day50/Tinder_Swiper/main.py
+++ Day50/Tinder_Swiper/main.py
@@ -5,6 +5,7 @@ from selenium import webdriver
 from selenium.webdriver.firefox.service import Service
 from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from webdriver_manager.firefox import GeckoDriverManager
@@ -14,8 +15,6 @@ from dotenv import load_dotenv
 load_dotenv()
 
 URL = 'https://tinder.com'
-USERNAME = os.getenv('GOOGLE_UN')
-PASSWORD = os.getenv('GOOGLE_PW')
 PROFILE = os.getenv('FIREFOX_PROFILE')
 
 
@@ -29,22 +28,16 @@ driver = webdriver.Firefox(
     ),
 )
 driver.get(URL)
-wait = WebDriverWait(driver=driver, timeout=10)
+wait = WebDriverWait(driver=driver, timeout=60)
 
-wait.until(EC.element_to_be_clickable((By.XPATH, '//span[text()="Log in"]')))
-driver.find_element(By.XPATH, '//span[text()="Log in"]').click()
+wait.until(EC.element_to_be_clickable((By.XPATH, '//span[text()="Personalize My Choices"]')))
+driver.find_element(By.XPATH, '//span[text()="Personalize My Choices"]').click()
 
-tinder_window = driver.current_window_handle
+wait.until(EC.element_to_be_clickable((By.XPATH, '//span[text()="Refuse All"]')))
+driver.find_element(By.XPATH, '//span[text()="Refuse All"]').click()
 
-time.sleep(1)
-driver.find_element(By.XPATH, '//span[text()="Log in with Google"]').click()
+wait.until(EC.element_to_be_clickable((By.XPATH, '/html/body/div[1]/div/div[1]/div/main/div[1]/div/div/div[1]/div[1]/div/div[contains(@class, "Isolate")]/div/div[2]/button')))
 
-wait.until(EC.number_of_windows_to_be(2))
-
-for window_handle in driver.window_handles:
-    if window_handle != tinder_window:
-        driver.switch_to.window(window_name=window_handle)
-        break
-
-time.sleep(1)
-driver.find_element(By.ID, 'identifierId').send_keys(USERNAME)
+while True:
+    main_page = driver.find_element(By.XPATH, '/html/body/div[1]/div/div[1]/div/main/div[1]/div/div/div[1]/div[1]/div/div[contains(@class, "Isolate")]/div/div[2]/button').click()
+    time.sleep(3)
