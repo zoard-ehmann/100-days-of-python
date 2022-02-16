e3be1afd3a2f7c810188edbba39d84745d9098dd
Zoárd Ehmann <zoard.ehmann@gmail.com>
2022-02-16 08:16:07 +0100

    Housekeeping

--- Day50/Tinder_Swiper/main.py
+++ Day50/Tinder_Swiper/main.py
@@ -5,7 +5,6 @@ from selenium import webdriver
 from selenium.webdriver.firefox.service import Service
 from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from webdriver_manager.firefox import GeckoDriverManager
