175d388e7e4438aa5417f59762801ccbb3d11355
Zoárd Ehmann <zoard.ehmann@gmail.com>
2022-02-17 13:41:10 +0100

    WIP: following followers

--- Day52/Instagram_Follower/insta_follower.py
+++ Day52/Instagram_Follower/insta_follower.py
@@ -2,6 +2,7 @@ import time
 
 from selenium import webdriver
 from selenium.webdriver.firefox.service import Service
+from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
@@ -10,23 +11,35 @@ from webdriver_manager.firefox import GeckoDriverManager
 
 class InstaFollower():
     
-    def __init__(self):
-        """Initialize Firefox webdriver and webdriver wait.
+    def __init__(self, firefox_profile: str=None):
+        """Initialize Firefox webdriver and webdriver wait. If profile is passed, skips log in session - assumes you are already logged in.
+
+        Args:
+            firefox_profile (str, optional): Firefox profile to use. Defaults to None.
         """
+        if firefox_profile == None:
+            options = None
+            self.skip_login = False
+        else:
+            options = Options()
+            options.profile = firefox_profile
+            self.skip_login = True
+
         self.driver = webdriver.Firefox(
+            options=options,
             service=Service(
                 executable_path=GeckoDriverManager(path='.wdm').install(),
                 log_path='.wdm/geckodriver.log'
             )
         )
+
         self.wait = WebDriverWait(driver=self.driver, timeout=60)
+        self.driver.get('https://www.instagram.com/')
 
     def __dismiss_prompt(self):
         """Dismisses Save Login and Turn On Notification prompts.
         """
-        save_login = '//button[text()="Not Now"]'
-        self.wait.until(EC.element_to_be_clickable((By.XPATH, save_login)))
-        self.driver.find_element(By.XPATH, save_login).click()
+        self.wait.until(EC.element_to_be_clickable((By.XPATH, '//button[text()="Not Now"]'))).click()
 
     def login(self, username: str, password: str):
         """Logs in to Instagram with the specified username and password combination.
@@ -35,23 +48,32 @@ class InstaFollower():
             username (str): Instagram username / phone number / email.
             password (str): Instagram password.
         """
-        self.driver.get('https://www.instagram.com/')
+        if not self.skip_login:
+            self.wait.until(EC.element_to_be_clickable((By.XPATH, '//button[text()="Accept All"]'))).click()
+            self.wait.until(EC.element_to_be_clickable((By.NAME, 'username'))).send_keys(username)
+            self.wait.until(EC.element_to_be_clickable((By.NAME, 'password'))).send_keys(password)
 
-        cookie_accept = '//button[text()="Accept All"]'
-        self.wait.until(EC.element_to_be_clickable((By.XPATH, cookie_accept)))
-        self.driver.find_element(By.XPATH, cookie_accept).click()
+            time.sleep(3)
+            self.driver.find_element(By.CSS_SELECTOR, 'button[type="submit"]').click()
 
-        self.driver.find_element(By.NAME, 'username').send_keys(username)
-        self.driver.find_element(By.NAME, 'password').send_keys(password)
+            for _ in range(2):
+                self.__dismiss_prompt()
 
-        time.sleep(3)
-        self.driver.find_element(By.CSS_SELECTOR, 'button[type="submit"]').click()
+    def find_followers(self, account: str):
+        """Finds all the followers of the target account.
 
-        for _ in range(2):
-            self.__dismiss_prompt()
-
-    def find_followers(self):
-        pass
+        Args:
+            account (str): Name of the account whose followers has to be followed.
+        """
+        self.wait.until(EC.element_to_be_clickable((By.XPATH, '//span[text()="Search"]'))).click()
+        self.driver.find_element(By.CSS_SELECTOR, 'input[placeholder="Search"]').send_keys(account)
+        self.wait.until(EC.element_to_be_clickable((By.XPATH, '/html/body/div[1]/section/nav/div[2]/div/div/div[2]/div[3]/div/div[2]/div/div[1]'))).click()
+        self.wait.until(EC.element_to_be_clickable((By.XPATH, '/html/body/div[1]/section/main/div/header/section/ul/li[2]'))).click()
 
     def follow(self):
-        pass
+        time.sleep(3)
+        window = '/html/body/div[6]/div/div/div/div[2]'
+        followers = '/html/body/div[6]/div/div/div/div[2]/ul/div/li'
+        followers = self.driver.find_elements(By.XPATH, '/html/body/div[6]/div/div/div/div[2]/ul/div/li')
+        for follower in followers:
+            print(follower.text)
--- Day52/Instagram_Follower/main.py
+++ Day52/Instagram_Follower/main.py
@@ -9,8 +9,11 @@ load_dotenv()
 INSTAGRAM_USERNAME = os.getenv('INSTAGRAM_UN')
 INSTAGRAM_PASSWORD = os.getenv('INSTAGRAM_PW')
 SIMILAR_ACCOUNT = os.getenv('ACCOUNT_TO_FOLLOW')
+FIREFOX_PROFILE = os.getenv('FF_PROFILE')
 
 
-insta_follower = InstaFollower()
+insta_follower = InstaFollower(firefox_profile=FIREFOX_PROFILE)
 
 insta_follower.login(username=INSTAGRAM_USERNAME, password=INSTAGRAM_PASSWORD)
+insta_follower.find_followers(account=SIMILAR_ACCOUNT)
+insta_follower.follow()
