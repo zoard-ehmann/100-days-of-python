e91653df35605d687d2b11ae4c977b2d42d91609
Zoárd Ehmann <zoard.ehmann@gmail.com>
2022-02-20 09:10:16 +0100

    Rendering HTML elements with Flask

--- Day54/Flask/hello.py
+++ Day54/Flask/hello.py
@@ -3,17 +3,23 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return '<h1 style="text-align: center;">Hello, World!</h1>' \
+    '<p>This is a kiscica.</p>' \
+    '<img src="https://media2.giphy.com/media/3oriO0OEd9QIDdllqo/giphy.gif?cid=ecf05e47u029f53b8529rmf4lxefgip8t1s3al6gtak4lv0s&rid=giphy.gif&ct=g" width="200px">'
+
 
 @app.route('/bye')
 def say_bye():
     return 'Bye!'
 
+
 @app.route('/username/<name>/<int:number>')
 def greet(name, number):
     return f'Hello {name.title()}, you are {number} years old!'
+
 
 if __name__ == '__main__':
     app.run(debug=True)
