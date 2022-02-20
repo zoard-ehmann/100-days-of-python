3a57fecdf9408b426da724b551090f4c6278813b
Zoárd Ehmann <zoard.ehmann@gmail.com>
2022-02-20 09:37:42 +0100

    Advanced Python decorator functions with *args and **kwargs

--- Day54/Python_Decorators/main.py
+++ Day54/Python_Decorators/main.py
@@ -71,3 +71,30 @@ def say_greeting():
     print("How are you?")
 decorated_function = delay_decorator(say_greeting)
 decorated_function()
+
+## Advanced Python Decorator Functions
+
+class User:
+
+    def __init__(self, name):
+        self.name = name
+        self.is_logged_in = False
+
+
+def is_authenticated_decorator(function):
+
+    def wrapper(*args, **kwargs):
+        if args[0].is_logged_in == True:
+            function(args[0])
+    
+    return wrapper
+
+
+@is_authenticated_decorator
+def create_blog_post(user):
+    print(f'This is {user.name}\'s new blog post.')
+
+
+new_user = User("zoard")
+new_user.is_logged_in = True
+create_blog_post(new_user)
