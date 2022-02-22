9c60bb1f2c20f706cfdc2523aafe76a0a444bec4
Zoárd Ehmann <zoard.ehmann@gmail.com>
2022-02-22 07:36:47 +0100

    Gender & age guessing page with Jinja

--- Day57/Jinja/server.py
+++ Day57/Jinja/server.py
@@ -1,6 +1,7 @@
 import random
 import datetime as dt
 
+import requests
 from flask import Flask, render_template
 
 
@@ -11,6 +12,19 @@ def home():
     random_number = random.randint(1, 10)
     curr_year = dt.datetime.today().strftime('%Y')
     return render_template('index.html', num=random_number, current_year=curr_year)
+
+@app.route('/guess/<name>')
+def guess(name):
+    with requests.Session() as session:
+        agify_response = session.get(f'https://api.agify.io/?name={name}')
+        agify_response.raise_for_status()
+        predicted_age = agify_response.json()['age']
+        
+        genderize_response = session.get(f'https://api.genderize.io?name={name}')
+        genderize_response.raise_for_status()
+        predicted_gender = genderize_response.json()['gender']
+
+    return render_template('guess.html', name=name.title(), age=predicted_age, gender=predicted_gender)
 
 if __name__ == '__main__':
     app.run(debug=True)
