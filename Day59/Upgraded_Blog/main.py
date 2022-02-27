0d0df41f638e8900884a164e325d04eb7879876a
Zoárd Ehmann <zoard.ehmann@gmail.com>
2022-02-27 14:48:50 +0100

    Blog solution by using custom class

--- Day59/Upgraded_Blog/main.py
+++ Day59/Upgraded_Blog/main.py
@@ -1,12 +1,23 @@
-from flask import Flask, render_template
+import datetime as dt
 
+import requests
+from flask import Flask, render_template
+from post import Post
+
+
+with requests.Session() as session:
+    response = session.get(url='https://api.npoint.io/8cb335cb2e29aa02528f')
+    response.raise_for_status()
+    data = response.json()
+
+all_posts = [Post(post) for post in data]
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('index.html', all_posts=all_posts)
 
 
 @app.route('/about')
@@ -17,6 +28,24 @@ def about():
 @app.route('/contact')
 def contact():
     return render_template('contact.html')
+
+
+@app.route('/post/<int:id>')
+def post(id):
+    requested_post = Post({
+        'id': -1,
+        'title': 'Uh-oh.. :(',
+        'subtitle': 'Post not found.',
+        'body': 'We cannot find the post you are looking for. Please double-check the URL you are referring to or feel free to get in touch with us.',
+        'date': dt.datetime.today().strftime('%Y-%m-%d'),
+        'author': 'AlertBot',
+    })
+
+    for post in all_posts:
+        if id == post.id:
+            requested_post = post
+
+    return render_template('post.html', post=requested_post)
 
 
 if __name__ == '__main__':
---
+++ Day59/Upgraded_Blog/post.py
@@ -0,0 +1,9 @@
+class Post:
+
+    def __init__(self, post: dict):
+        self.id = post['id']
+        self.title = post['title']
+        self.subtitle = post['subtitle']
+        self.body = post['body']
+        self.date = post['date']
+        self.author = post['author']
