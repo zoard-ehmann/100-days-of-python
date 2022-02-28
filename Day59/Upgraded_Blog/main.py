812c5e7b9d8e23fd8cfb092e86d8e58cbcd74237
Zoárd Ehmann <zoard.ehmann@gmail.com>
2022-02-28 18:08:16 +0100

    Set up contact form for the blog site

--- Day59/Upgraded_Blog/main.py
+++ Day59/Upgraded_Blog/main.py
@@ -1,7 +1,18 @@
+import os
+import smtplib
 import datetime as dt
 
 import requests
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+EMAIL = os.getenv('MY_EMAIL')
+PASSWORD = os.getenv('MY_PASSWORD')
+HOST = os.getenv('SMTP_HOST')
+PORT = os.getenv('SMTP_PORT')
 
 
 with requests.Session() as session:
@@ -22,9 +33,14 @@ def about():
     return render_template('about.html')
 
 
-@app.route('/contact')
+@app.route('/contact', methods=['GET', 'POST'])
 def contact():
-    return render_template('contact.html')
+    msg_sent = False
+
+    if request.method == 'POST':
+        msg_sent = send_mail(form_data=request.form)
+
+    return render_template('contact.html', msg_sent=msg_sent)
 
 
 @app.route('/post/<int:id>')
@@ -43,6 +59,32 @@ def post(id):
             requested_post = post
 
     return render_template('post.html', post=requested_post)
+
+
+def send_mail(form_data: dict) -> bool:
+    """Takes the data from the contact form and sends the message to the owner's email.
+
+    Args:
+        form_data (dict): Data from the contact form.
+
+    Returns:
+        bool: Returns True once the functions runs successfully.
+    """
+    with smtplib.SMTP(host=HOST, port=PORT) as smtp_session:
+        smtp_session.starttls()
+        smtp_session.login(user=EMAIL, password=PASSWORD)
+        smtp_session.sendmail(
+            from_addr=EMAIL,
+            to_addrs=EMAIL,
+            msg='Subject:New Message from My Blog\n\n'
+            f'Name: {form_data["name"]}\n'
+            f'Email: {form_data["email"]}\n'
+            f'Phone: {form_data["phone"]}\n'
+            f'Message: {form_data["message"]}\n'
+            .encode("utf-8")
+        )
+
+    return True
 
 
 if __name__ == '__main__':
