9212a7247812844b78d537fab72d7b850a54f13f
Zoárd Ehmann <zoard.ehmann@gmail.com>
2022-03-02 07:23:56 +0100

    Rendering all cafes in a Bootstrap table

--- Day62/Coffee_and_Wifi/main.py
+++ Day62/Coffee_and_Wifi/main.py
@@ -42,7 +42,7 @@ def add_cafe():
 
 @app.route('/cafes')
 def cafes():
-    with open('cafe-data.csv', newline='') as csv_file:
+    with open('Day62/Coffee_and_Wifi/cafe-data.csv', newline='') as csv_file:
         csv_data = csv.reader(csv_file, delimiter=',')
         list_of_rows = []
         for row in csv_data:
