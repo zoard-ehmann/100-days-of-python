5fe47e7e454ed8f25b9a58af8118a92b3d8967d5
Zoárd Ehmann <zoard.ehmann@gmail.com>
2022-03-03 16:54:16 +0100

    SQLite Management with SQLite3

---
+++ Day63/SQLite/main.py
@@ -0,0 +1,16 @@
+import sqlite3
+
+
+db = sqlite3.connect(database='Day63/SQLite/books-collection.db')
+cursor = db.cursor()
+
+# INFO: Create table
+# cursor.execute(
+#     "CREATE TABLE books (id INTEGER PRIMARY KEY, title varchar(250) NOT NULL UNIQUE, author varchar(250) NOT NULL, rating FLOAT NOT NULL)"
+# )
+
+# INFO: Insert data
+cursor.execute(
+    "INSERT INTO books VALUES(1, 'Harry Potter', 'J.K. Rowling', '9.5')"
+)
+db.commit()
