d7da015ddb5f8ec5c38129df376f3af4993ff107
Zoárd Ehmann <zoard.ehmann@gmail.com>
2022-03-03 08:48:34 +0100

    Add and render books in the library

--- Day63/Databases/main.py
+++ Day63/Databases/main.py
@@ -7,12 +7,19 @@ all_books = []
 
 @app.route('/')
 def home():
-    pass
+    return render_template('index.html', library=all_books)
 
 
-@app.route("/add")
+@app.route("/add", methods=['GET', 'POST'])
 def add():
-    pass
+    if request.method == 'POST':
+        all_books.append({
+            'title': request.form['book_name'],
+            'author': request.form['book_author'],
+            'rating': request.form['book_rating']
+        })
+        return redirect(url_for('home'))
+    return render_template('add.html')
 
 
 if __name__ == "__main__":
