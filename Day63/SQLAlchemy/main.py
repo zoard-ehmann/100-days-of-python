e5fbe94a81693303d6fc3433c84eba4547887bba
Zoárd Ehmann <zoard.ehmann@gmail.com>
2022-03-03 17:10:07 +0100

    CRUD Operations with SQLAlchemy

--- Day63/SQLAlchemy/main.py
+++ Day63/SQLAlchemy/main.py
@@ -17,6 +17,31 @@ class Books(db.Model):
 
 if __name__ == '__main__':
     db.create_all()
-    hp = Books(title='Harry Potter', author='J.K. Rowling', rating=9.4)
-    db.session.add(hp)
-    db.session.commit()
+
+    # INFO: Create new record
+    # book = Books(title='Harry Potter', author='J.K. Rowling', rating=9.4)
+    # db.session.add(book)
+    # db.session.commit()
+
+    # INFO: Read all records
+    # all_books = db.session.query(Books).all()
+    
+    # INFO: Read a particular record by query
+    # book = Books.query.filter_by(title='Harry Potter').first()
+    
+    # INFO: Update a particular record by query
+    # book_to_update = Books.query.filter_by(title='Harry Potter').first()
+    # book_to_update.title = 'Harry Potter and the Chamber of Secrets'
+    # db.session.commit()
+
+    # INFO: Update a record by primary key
+    # book_id = 1
+    # book_to_update = Books.query.get(book_id)
+    # book_to_update.title = 'Harry Potter and the Chamber of Secrets'
+    # db.session.commit()
+
+    # INFO: Delete a particular record by primary key
+    # book_id = 1
+    # book_to_delete = Books.query.get(book_id)
+    # db.session.delete(book_to_delete)
+    # db.session.commit()
