0a77179051fd6452a48177f5d7194afd794978a0
Zoárd Ehmann <zoard.ehmann@gmail.com>
2022-03-04 07:34:40 +0100

    Adding base movie to the DB for testing

--- Day64/My_Top_10_Movies/main.py
+++ Day64/My_Top_10_Movies/main.py
@@ -13,13 +13,40 @@ from dotenv import load_dotenv
 load_dotenv()
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.getenv('APP_SECRET')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('SQLITE_DB_PATH')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 Bootstrap(app)
+db = SQLAlchemy(app)
 
 
-@app.route("/")
+class Movie(db.Model):
+    id = db.Column(db.Integer, primary_key=True, nullable=False)
+    title = db.Column(db.String, nullable=False)
+    year = db.Column(db.Integer, nullable=False)
+    description = db.Column(db.String(250), nullable=False)
+    rating = db.Column(db.Float, nullable=False)
+    ranking = db.Column(db.Integer, nullable=False, unique=True)
+    review = db.Column(db.String(250), nullable=False)
+    img_url = db.Column(db.String, nullable=False, unique=True)
+
+
+@app.route('/')
 def home():
-    return render_template("index.html")
+    all_movies = Movie.query.all()
+    return render_template('index.html', all_movies=all_movies)
 
 
 if __name__ == '__main__':
+    # db.create_all()
+    # new_movie = Movie(
+    #     title="Phone Booth",
+    #     year=2002,
+    #     description="Publicist Stuart Shepard finds himself trapped in a phone booth, pinned down by an extortionist's sniper rifle. Unable to leave or receive outside help, Stuart's negotiation with the caller leads to a jaw-dropping climax.",
+    #     rating=7.3,
+    #     ranking=10,
+    #     review="My favourite character was the caller.",
+    #     img_url="https://image.tmdb.org/t/p/w500/tjrX2oWRCM3Tvarz38zlZM7Uc10.jpg"
+    # )
+    # db.session.add(new_movie)
+    # db.session.commit()
     app.run(debug=True)
