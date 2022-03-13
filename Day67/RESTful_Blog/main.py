9e9001f52e90524de28705e8749caa58ec70ab12
Zoárd Ehmann <zoard.ehmann@gmail.com>
2022-03-13 10:54:47 +0100

    Implement delete post method

--- Day67/RESTful_Blog/main.py
+++ Day67/RESTful_Blog/main.py
@@ -53,9 +53,9 @@ def get_all_posts():
     return render_template('index.html', all_posts=posts)
 
 
-@app.route('/post/<int:index>')
-def show_post(index):
-    requested_post = db.session.query(BlogPost).get(index)
+@app.route('/post/<int:post_id>')
+def show_post(post_id):
+    requested_post = db.session.query(BlogPost).get(post_id)
     return render_template('post.html', post=requested_post)
 
 
@@ -103,8 +103,16 @@ def edit_post(post_id):
         post.img_url = edit_form.img_url.data
         post.body = bleach.clean(edit_form.body.data, tags=ALLOWED_TAGS)
         db.session.commit()
-        return redirect(url_for('show_post', index=post_id))
+        return redirect(url_for('show_post', post_id=post_id))
     return render_template('make-post.html', form=edit_form)
+
+
+@app.route('/delete/<int:post_id>')
+def delete_post(post_id):
+    post = db.session.query(BlogPost).get(post_id)
+    db.session.delete(post)
+    db.session.commit()
+    return redirect(url_for('get_all_posts'))
 
 
 if __name__ == '__main__':
