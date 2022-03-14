76ce4a4ea6dea4bf24ed165d262676b489aa9b51
Zoárd Ehmann <zoard.ehmann@gmail.com>
2022-03-14 21:10:15 +0100

    Register users to the DB

--- Day69/Blog_with_Users/main.py
+++ Day69/Blog_with_Users/main.py
@@ -1,6 +1,6 @@
 import os
 
-from flask import Flask, render_template, redirect, url_for, flash
+from flask import Flask, render_template, redirect, request, url_for, flash
 from flask_bootstrap import Bootstrap
 from flask_ckeditor import CKEditor
 from datetime import date
@@ -8,25 +8,34 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import relationship
 from flask_login import UserMixin, login_user, LoginManager, login_required, current_user, logout_user
-from forms import CreatePostForm
+from forms import CreatePostForm, RegisterForm
 from flask_gravatar import Gravatar
 from dotenv import load_dotenv
 
 
+#INFO: CONSTANTS
 load_dotenv()
+HASH_METHOD = 'pbkdf2:sha256'
+SALT_LENGTH = 16
+
+#INFO: INITIALIZE APP
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.getenv('APP_SECRET')
 ckeditor = CKEditor(app)
 Bootstrap(app)
 
-##CONNECT TO DB
+#INFO: SET UP LOGIN MANAGER
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+
+#INFO: CONNECT TO DB
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 
-##CONFIGURE TABLES
-
+#INFO: CONFIGURE TABLES
 class BlogPost(db.Model):
     __tablename__ = 'blog_posts'
     id = db.Column(db.Integer, primary_key=True)
@@ -36,18 +45,41 @@ class BlogPost(db.Model):
     date = db.Column(db.String(250), nullable=False)
     body = db.Column(db.Text, nullable=False)
     img_url = db.Column(db.String(250), nullable=False)
-db.create_all()
 
 
+class User(UserMixin, db.Model):
+    __tablename__ = 'user'
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String, unique=True, nullable=False)
+    password = db.Column(db.String, nullable=False)
+    name = db.Column(db.String, nullable=False)
+
+
+#INFO: USER LOADER FOR LOGIN MANAGER
+@login_manager.user_loader
+def load_user(id):
+    return db.session.query(User).get(id)
+
+
+#INFO: APP ROUTES
 @app.route('/')
 def get_all_posts():
     posts = BlogPost.query.all()
     return render_template('index.html', all_posts=posts)
 
 
-@app.route('/register')
+@app.route('/register', methods=['GET', 'POST'])
 def register():
-    return render_template('register.html')
+    register_form = RegisterForm()
+    if register_form.validate_on_submit():
+        db.session.add(User(
+            email=register_form.email.data,
+            password=generate_password_hash(password=register_form.password.data, method=HASH_METHOD, salt_length=SALT_LENGTH),
+            name=register_form.name.data
+        ))
+        db.session.commit()
+        return redirect(url_for('get_all_posts'))
+    return render_template('register.html', form=register_form)
 
 
 @app.route('/login')
@@ -124,5 +156,7 @@ def delete_post(post_id):
     return redirect(url_for('get_all_posts'))
 
 
+#INFO: START APP
 if __name__ == '__main__':
+    db.create_all()
     app.run(host='127.0.0.1', port=5000, debug=True)
