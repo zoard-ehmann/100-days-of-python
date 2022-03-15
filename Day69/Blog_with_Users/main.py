a088c5e1034224a6836237cc4b5c5a05684ae653
Zoárd Ehmann <zoard.ehmann@gmail.com>
2022-03-15 12:21:02 +0100

    Fix title on post edits

--- Day69/Blog_with_Users/main.py
+++ Day69/Blog_with_Users/main.py
@@ -219,7 +219,7 @@ def edit_post(post_id):
         db.session.commit()
         return redirect(url_for('show_post', post_id=post.id))
 
-    return render_template('make-post.html', form=edit_form)
+    return render_template('make-post.html', form=edit_form, is_edit=True)
 
 
 @app.route('/delete/<int:post_id>')
